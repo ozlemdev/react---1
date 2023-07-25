cd4aae2e42f9a6547d5391b479188690d167b69c
ozlem <06ozlemozturk@gmail.com>
2023-07-25 22:41:13 +0300

    state

--- src/App.js
+++ src/App.js
@@ -3,32 +3,19 @@ import Berkant from "./components/Berkant";
 import Deneme from "./components/Deneme";
 import { useState } from "react";
 function App() {
-  const [color, setColor] = useState("blue");
+  const [status, setStatus] = useState(false);
 
-  const deneme = "props mantıgı";
-  const arr = [
-    { name: "array1", id: 0 },
-    { name: "array2", id: 1 },
-    { name: "array3", id: 2 },
-  ];
-  console.log(arr, "arr");
-  const clickFunc = () => {
-    setColor("red");
-  };
-
-  return (
-    <div className="App">
-      {/*component mantıgı */}
-      <Berkant berkant={deneme} />
-      <Deneme />
-
-      {arr.map((ar, i) => (
-        <div key={ar.id}> {ar.name} </div>
-      ))}
-      <div style={{ color: color }}>berkant</div>
-      <button onClick={clickFunc}>tıkla</button>
-    </div>
-  );
+  if (status) {
+    return <div>statu durumu true</div>;
+  } else {
+    return (
+      <div>
+        satatu durumu false
+        <button onClick={() => setStatus(!status)}>tıkla</button>
+      </div>
+    );
+  }
 }
+
 //* component mantığı */
 export default App;
